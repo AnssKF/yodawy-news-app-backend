f9c0bed85cab30e95103c87821fd470338cfd5f3
AnssK <a.khaled@yodawy.com>
2020-11-09 16:00:21 +0200

    Style FormField based on added css variables / remove nova classes

--- src/nova-components/FavCreation/resources/js/components/custom/FormField/FormField.vue
+++ src/nova-components/FavCreation/resources/js/components/custom/FormField/FormField.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="c-ff-group mb-2 mt-4">
+    <div class="c-ff-group">
         <input v-bind="$attrs" ref="inputfield" @blur="$emit('blur')" @input="$emit('input', $refs.inputfield.value)" :value="value"/>
         <span class="bar"></span>
         <label :class="danger? 'text-danger': 'text-primary'">
             <slot></slot>
         </label>
 
-        <span v-if="danger" class="mt-2 text-danger help-text">{{ helpText }}</span>
+        <span v-if="danger" class="help-text">{{ helpText }}</span>
     </div>
 </template>
 
