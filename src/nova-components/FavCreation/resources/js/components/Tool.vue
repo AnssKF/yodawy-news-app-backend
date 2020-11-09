7eafd30858e7a66db1555ae2c5c975b7982a9e88
AnssK <a.khaled@yodawy.com>
2020-11-09 15:58:54 +0200

    Define required css variables for fav-creation-tool
    -- Added margin/padding variables
    -- Added colors variables

--- src/nova-components/FavCreation/resources/js/components/Tool.vue
+++ src/nova-components/FavCreation/resources/js/components/Tool.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
+    <div class="fav-creation-tool">
         <FavCreationForm />
     </div>
 </template>
 
 <script>
 import FavCreationForm from './Fav/FavCreationForm.vue';
+
+import './Tool.css'
 
 export default {
     components: {
