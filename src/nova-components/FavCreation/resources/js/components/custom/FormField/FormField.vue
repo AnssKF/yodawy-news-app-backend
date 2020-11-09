0561f573e8ef1aa465540d3d5e806576ab06f2d1
AnssK <a.khaled@yodawy.com>
2020-11-09 14:26:13 +0200

    Modified propes in FormField component
    -- Added Prop type and default value

--- src/nova-components/FavCreation/resources/js/components/custom/FormField/FormField.vue
+++ src/nova-components/FavCreation/resources/js/components/custom/FormField/FormField.vue
@@ -2,21 +2,32 @@
     <div class="c-ff-group mb-2 mt-4">
         <input v-bind="$attrs" ref="inputfield" @blur="$emit('blur')" @input="$emit('input', $refs.inputfield.value)" :value="value"/>
         <span class="bar"></span>
-        <label :class="!!danger? 'text-danger': 'text-primary'">
+        <label :class="danger? 'text-danger': 'text-primary'">
             <slot></slot>
         </label>
 
-        <span v-if="!!danger" class="mt-2 text-danger help-text">{{ helpText }}</span>
+        <span v-if="danger" class="mt-2 text-danger help-text">{{ helpText }}</span>
     </div>
 </template>
 
 <script>
+import './FormField.css';
+
 export default {
     name: 'FormField',
-    props: ['value', 'danger', 'help-text']
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+        'danger': {
+            type: Boolean,
+            default: false
+        },
+        'help-text': {
+            type: String,
+            default: 'Please insert a valid data.'
+        },
+    }
 }
 </script>
-
-<style scoped>
- @import './FormField.css';
-</style>
