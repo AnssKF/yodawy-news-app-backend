671508fc599dd9670effd960f00888a976883522
AnssK <a.khaled@yodawy.com>
2020-11-11 17:19:18 +0200

    Added vuex to FavCreationTool
    -- Added initial stores for fav/users

--- src/nova-components/FavCreation/resources/js/tool.js
+++ src/nova-components/FavCreation/resources/js/tool.js
@@ -1,8 +1,11 @@
 import FavCreationForm from './components/Fav/FavCreationForm.vue';
 import FormField from './components/custom/FormField/FormField.vue';
 
+import { registerStoreModules } from './store'
+
 Nova.booting((Vue, router, store) => {
   registerComponents(Vue);
+  registerStoreModules(store);
 
   router.addRoutes([
     {
