5ebd0ddaea62312979fa9dd04cae92d44e2b75eb
AnssK <a.khaled@yodawy.com>
2020-11-11 17:20:37 +0200

    Add search user state/action to user store

--- src/nova-components/FavCreation/resources/js/store/modules/user-store/index.js
+++ src/nova-components/FavCreation/resources/js/store/modules/user-store/index.js
@@ -1,12 +1,23 @@
+import { fetchUsers } from './actions'
+
 const state = {
+    availableUsers: [],
+
+    searchCancelTokenSource: null,
 }
+
 const getters = {
+    getAvailableUsers: store => store.availableUsers, 
 }
 
 const actions = {
+    fetchUsers
 }
 
 const mutations = {
+    updateSearchCancelTokenSource: (store, source) => store.searchCancelTokenSource = source,
+
+    setAvailableUsers: (store, availableUsers) => store.availableUsers = availableUsers
 }
 
 export default {
