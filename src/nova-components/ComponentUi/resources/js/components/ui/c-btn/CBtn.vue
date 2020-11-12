66271284e1307485260d9c3ca75ea77c32424f3f
AnssK <a.khaled@yodawy.com>
2020-11-12 16:33:23 +0200

    Added CBtn component

---
+++ src/nova-components/ComponentUi/resources/js/components/ui/c-btn/CBtn.vue
@@ -0,0 +1,45 @@
+<template>
+    <button 
+        @click="handleOnClick"
+        v-bind="$attrs"
+        class="c-btn c-btn-shadow-effect"
+        :class="getBtnStyle">
+        
+        <slot>Submit</slot>
+    </button>
+</template>
+
+<script>
+import './CBtn.css';
+
+export default {
+    name: 'c-btn',
+    props: {
+        styleType: {
+            type: String,
+            default: 'primary',
+            // validator: function (value) {
+            //     return ['primary', 'danger'].indexOf(value) !== -1
+            // }
+        },
+        onClick: {
+            type: Function,
+            default: null
+        }
+    },
+    computed: {
+        getBtnStyle() {
+            return this.styleType === 'danger'? 'btn-danger': 'btn-primary';
+        }
+    },
+    methods: {
+        handleOnClick($event){
+            if(this.onClick === null) return
+
+            if(!(this.onClick instanceof Function)) throw ('onClick prop should be instance of Function')
+
+            this.onClick($event)
+        }
+    }
+}
+</script>
