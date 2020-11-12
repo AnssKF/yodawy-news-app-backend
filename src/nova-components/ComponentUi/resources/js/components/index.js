36738930f9814cca0783227404ea7ec12652de5a
AnssK <a.khaled@yodawy.com>
2020-11-12 16:33:23 +0200

    Added CBtn component

--- src/nova-components/ComponentUi/resources/js/components/index.js
+++ src/nova-components/ComponentUi/resources/js/components/index.js
@@ -1,10 +1,12 @@
 import FormField from './form/form-field/FormField.vue';
 import CCard from './ui/c-card/CCard.vue'
+import CBtn from './ui/c-btn/CBtn.vue'
 
 export const registerComponents = (Vue) => {
     const components = [
         FormField,
-        CCard
+        CCard,
+        CBtn
     ];
 
     components.forEach( component => Vue.component(component.name, component));
