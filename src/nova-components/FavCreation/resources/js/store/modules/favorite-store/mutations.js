e100f5d75a4a300889757396e05044473bd1655e
AnssK <a.khaled@yodawy.com>
2020-11-12 13:50:59 +0200

    Add author field to favForm store

--- src/nova-components/FavCreation/resources/js/store/modules/favorite-store/mutations.js
+++ src/nova-components/FavCreation/resources/js/store/modules/favorite-store/mutations.js
@@ -1,3 +1,4 @@
+import { FAV_FORM_INITIAL } from './state'
 
 export const UPDATE_FAV_FORM_FIELD = 'UPDATE_FORM_FIELD';
 const updateFormField = (state, { field, attr, value }) => {
@@ -6,11 +7,7 @@ const updateFormField = (state, { field, attr, value }) => {
 
 export const RESET_FAV_FORM = 'RESET_FAV_FORM';
 const resetFavForm = (state) => {
-    state.favForm = {
-        url: { value: '', touched: false },
-        publishedAt: { value: '', touched: false },
-        user: { value: '', display: '', touched: false, userSearch: '' }
-    }
+    state.favForm = FAV_FORM_INITIAL()
 }
 
 export default {
