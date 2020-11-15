e74f4127060eb8061367bd7e17caa90d2147a66d
AnssK <a.khaled@yodawy.com>
2020-11-15 16:24:30 +0200

    Refactor FavStore validation getters to use other getters

--- src/nova-components/FavCreation/resources/js/store/modules/favorite-store/getters.js
+++ src/nova-components/FavCreation/resources/js/store/modules/favorite-store/getters.js
@@ -7,7 +7,7 @@ export default {
     getUserField: state => state.favForm.user,
     getAuthorField: state => state.favForm.author,
 
-    invalidUrl: state =>  (state.favForm.url.value === '' || !state.favForm.url.value.match(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/)),
-    invalidPublishedAt: state => (state.favForm.publishedAt.value === '' || !state.favForm.publishedAt.value.match(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/)),
-    invalidUser: state => (state.favForm.user.value === '')
+    invalidUrl: (state, { getUrlField }) =>  (getUrlField.value === '' || !getUrlField.value.match(/[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/)),
+    invalidPublishedAt: (state, { getPublishedAtField }) => (getPublishedAtField.value === '' || !getPublishedAtField.value.match(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/)),
+    invalidUser: (state, { getUserField }) => (getUserField.value === '')
 }
