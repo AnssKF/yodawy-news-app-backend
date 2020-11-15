402222128515ffface183e0a4f7ac87d394e3e74
AnssK <a.khaled@yodawy.com>
2020-11-15 13:38:22 +0200

    Fix duplicates after rebase

--- src/nova-components/FavCreation/resources/js/store/modules/favorite-store/mutations.js
+++ src/nova-components/FavCreation/resources/js/store/modules/favorite-store/mutations.js
@@ -10,16 +10,6 @@ const resetFavForm = (state) => {
     state.favForm = FAV_FORM_INITIAL()
 }
 
-export const UPDATE_FAV_FORM_FIELD = 'UPDATE_FORM_FIELD';
-const updateFormField = (state, { field, attr, value }) => {
-    state.favForm[field][attr] = value;
-}
-
-export const RESET_FAV_FORM = 'RESET_FAV_FORM';
-const resetFavForm = (state) => {
-    state.favForm = FAV_FORM_INITIAL()
-}
-
 export default {
     [UPDATE_FAV_FORM_FIELD]: updateFormField,
     [RESET_FAV_FORM]: resetFavForm
