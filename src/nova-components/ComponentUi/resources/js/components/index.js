3eab0bf2805f250d9af8a05474cb40682c75b9ad
AnssK <a.khaled@yodawy.com>
2020-11-17 14:05:12 +0200

    Added Paginator component to ComponentUi tool

--- src/nova-components/ComponentUi/resources/js/components/index.js
+++ src/nova-components/ComponentUi/resources/js/components/index.js
@@ -1,12 +1,14 @@
 import FormField from './form/form-field/FormField.vue';
-import CCard from './ui/c-card/CCard.vue'
-import CBtn from './ui/c-btn/CBtn.vue'
+import CCard from './ui/c-card/CCard.vue';
+import CBtn from './ui/c-btn/CBtn.vue';
+import Paginator from './ui/paginator/Paginator.vue'
 
 export const registerComponents = (Vue) => {
     const components = [
         FormField,
         CCard,
-        CBtn
+        CBtn,
+        Paginator
     ];
 
     components.forEach( component => Vue.component(component.name, component));
---
+++ src/nova-components/ComponentUi/resources/js/components/ui/paginator/Paginator.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="paginator">
+        <a class="paginator-btn previous" :class="getPrevClassList" @click="handelOnPrev">&laquo; Previous</a>
+        <a class="paginator-btn next" :class="getNextClassList" @click="handelOnNext">Next &raquo;</a>
+    </div>
+</template>
+
+<script>
+import './Paginator.css'
+export default {
+    name: 'paginator',
+    props: {
+        disabledPrevioud: {
+            type: Boolean,
+            default: null
+        },
+        disabledNext: {
+            type: Boolean,
+            default: null
+        },
+        onNext: {
+            type: Function,
+            default: null
+        },
+        onPrevious: {
+            type: Function,
+            default: null
+        }
+    },
+    computed: {
+        getPrevClassList() {
+            return {
+                'paginator-btn previous': true,
+                'disabled': this.disabledPrevioud
+            }
+        },
+
+        getNextClassList() {
+            return {
+                'paginator-btn next': true,
+                'disabled': this.disabledNext
+            }
+        }
+    },
+    methods: {
+        handelOnPrev() {
+            if(this.onPrevious === null || this.disabledPrevioud) return
+            if(! this.onPrevious instanceof Function) throw 'onPrevioes Prop should be instance of Function';
+            this.onPrevious()
+        },
+
+        handelOnNext() {
+            if(this.onNext === null || this.disabledNext) return
+            if(! this.onNext instanceof Function) throw 'onPrevioes Prop should be instance of Function';
+            this.onNext()
+        }
+    }
+}
+</script>
