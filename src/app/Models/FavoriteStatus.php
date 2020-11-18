3efb0206b4f289f25aa7ec320b6a82b071e8a7ca
AnssK <a.khaled@yodawy.com>
2020-11-18 11:01:08 +0200

    Refactor FavoriteStatus add fields into fillable array

--- src/app/Models/FavoriteStatus.php
+++ src/app/Models/FavoriteStatus.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use \Venturecraft\Revisionable\RevisionableTrait;
 
 class FavoriteStatus extends Model
 {
-    use HasFactory;
+    use HasFactory, RevisionableTrait;
+
+    protected $fillable = [
+        'favorite_id',
+        'posted'
+    ];
 
     protected $casts = [
         'posted' => 'boolean',
