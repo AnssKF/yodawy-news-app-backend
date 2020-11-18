71b65007c24be1ec7d4990b8c57d736fa5d5b13e
AnssK <a.khaled@yodawy.com>
2020-11-18 12:21:36 +0200

    Added enconding method to FavHelpers to encode DateRanfeFilters into base64

--- src/nova-components/MyFavorites/resources/js/helpers/FavoritesHelper.js
+++ src/nova-components/MyFavorites/resources/js/helpers/FavoritesHelper.js
@@ -1,21 +1,40 @@
+import { filter } from "lodash";
 
 export class FavoritesHelper {
-    
+
     static parseFavoritesResources(resources) {
         const favs = resources.reduce((oldFavsAcc, currentResource) => {
             const fav = currentResource.fields.reduce((oldFieldsAcc, currentField) => {
-                let attr = currentField.attribute === 'ComputedField'? currentField.name.toLowerCase(): currentField.attribute;
-                
-                if(attr.indexOf('.') !== -1){
+                let attr = currentField.attribute === 'ComputedField' ? currentField.name.toLowerCase() : currentField.attribute;
+
+                if (attr.indexOf('.') !== -1) {
                     const split = attr.split('.')
-                    attr = split[split.length -1]
+                    attr = split[split.length - 1]
                 }
 
-                return { ...oldFieldsAcc,  [attr]: currentField.value }
+                return { ...oldFieldsAcc, [attr]: currentField.value }
             }, {})
             return [...oldFavsAcc, fav]
         }, [])
-        
+
         return favs;
+    }
+
+    static encodeDateFromToFilter({dateFrom, dateTo}) {
+        const filters = []
+
+        if(dateFrom)
+            filters.push({ 
+                class: "App\\Nova\\Filters\\FavoriteDateFromFilter", 
+                value: dateFrom
+            })
+
+        if(dateTo)
+            filters.push({ 
+                class: "App\\Nova\\Filters\\FavoriteDateToFilter", 
+                value: dateTo
+            })
+        
+        return btoa(JSON.stringify(filters))
     }
 }
